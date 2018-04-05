3d59bf98a8e65e692df58fbf04dba3dee8cf4484
Nan-L <nanliuca@outlook.com>
2018-04-04 22:36:18 -0700

    added autofill functionality and displayed content preview in book show page

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,10 +5,17 @@ class Book < ApplicationRecord
   belongs_to :user, optional:true
   validates :title,  presence: true, length: { maximum: 50 }
   validates :year, presence: true, length: { is: 4 }, numericality: { only_integer: true}
-  validates :isbn, presence: true, length: { is: 13 }, numericality: { only_integer: true}
+  validates :isbn, presence: true, numericality: { only_integer: true}
   validates :author, presence: true, length: { maximum: 100 }
   validates :price, presence: true, length: { maximum: 10 }, numericality: true
   validates :course_number, presence: false, length: { maximum: 10 }
   validates :image, presence: false
+  validate :check_length
+
+  def check_length
+    unless isbn.size == 10 or isbn.size == 13
+      errors.add(:isbn, "length must be 10 or 13") 
+    end
+  end
 
 end
